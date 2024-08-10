7e5c634c0a034c5f60a1b5b1533828bc0e384e31
kunalkaushik1982 <kunal.kaushik1982@gmail.com>
2024-08-10 17:02:35 +0530

    Setup for AWS Beanstalk deploymnet

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -87,9 +87,7 @@ class ModelTrainer:
             #To get best model score from dict
             best_model_score=max(sorted(model_report.values()))
             #To get best model name from dict
-            best_model_name=list(model_report.keys())[
-                list(model_report.values()).index(best_model_score)
-            ]
+            best_model_name=list(model_report.keys())[list(model_report.values()).index(best_model_score)]
             
             best_model=models[best_model_name]
             if best_model_score<0.6:
